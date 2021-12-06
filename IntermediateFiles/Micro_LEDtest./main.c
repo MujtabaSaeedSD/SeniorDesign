7f9447c8e47e76e7d702076cb5cae844ac70d2ed
MujtabaSaeedSD <95595895+MujtabaSaeedSD@users.noreply.github.com>
2021-12-05 21:13:56 -0600

    Add files via upload

--- IntermediateFiles/Micro_LEDtest./main.c
+++ IntermediateFiles/Micro_LEDtest./main.c
@@ -1,1 +1,37 @@
 
+#define F_CPU 16000000UL
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	
+    DDRB |= 1 << PINB7;
+	
+    while (1) 
+    {
+		// testing different timings to ensure proper communication between micro controller and code
+		PORTB |= 1 <<PINB7;
+		_delay_ms(1000);
+		PORTB &= ~(1 <<PINB7);
+		_delay_ms(1000);
+		
+		
+		PORTB |= 1 <<PINB7;
+		_delay_ms(100);
+		PORTB &= ~(1 <<PINB7);
+		_delay_ms(100);
+		
+		
+		
+		PORTB |= 1 <<PINB7;
+		_delay_ms(500);
+		PORTB &= ~(1 <<PINB7);
+		_delay_ms(500);
+		
+    }
+}
+
